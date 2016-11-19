2bc5e1f9380c087ac627ebaf5a36ba2dab358251
guni973 <ad2314ce71926@gmail.com>
2016-11-19 22:36:46 +0900

    mv init.el

--- .emacs.d/elpa/auto-complete-1.4/auto-complete-autoloads.el
+++
@@ -1,16 +0,0 @@
-;;; auto-complete-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
-
-;;;### (autoloads nil nil ("auto-complete-config.el" "auto-complete-pkg.el"
-;;;;;;  "auto-complete.el") (22574 13326 308948 50000))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; auto-complete-autoloads.el ends here
