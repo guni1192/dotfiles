6b7d5a1584e385d91380a839fdaedfbd56ed5b3c
guni973 <ad2314ce71926@gmail.com>
2016-12-16 16:30:40 +0900

    add tranceparency setting

--- init.el
+++ init.el
@@ -225,4 +225,17 @@
     (ding)))
 (setq ring-bell-function 'my-bell-function)
 
+;; Color
+(if window-system (progn
+    (set-background-color "Black")
+    (set-foreground-color "LightGray")
+    (set-cursor-color "Gray")
+    (set-frame-parameter nil 'alpha 70) ;透明度
+    ))
 
+;; 透明度を変更するコマンド M-x set-alpha
+;; http://qiita.com/marcy@github/items/ba0d018a03381a964f24
+(defun set-alpha (alpha-num)
+  "set frame parameter 'alpha"
+  (interactive "nAlpha: ")
+  (set-frame-parameter nil 'alpha (cons alpha-num '(90))))
