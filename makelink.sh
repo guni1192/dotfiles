8f810df89146161bd6166f544f2d4325028a5168
guni973 <ad2314ce71926@gmail.com>
2017-03-09 03:38:16 +0900

    xmodmap added

--- makelink.sh
+++ makelink.sh
@@ -8,6 +8,8 @@ if [ "$(expr substr $(uname -s) 1 5)" == 'Linux' ]; then
     git clone https://github.com/dimitri/el-get.git ~/.emacs.d/el-get/
     ln -s ~/dotfiles/init.el ~/.emacs.d/init.el
     ln -s ~/dotfiles/.tmux.conf ~/.tmux.conf
+    # If use ThinkPad, .Xmodmap added
+    # ln -s /home/guni/dotfiles/.Xmodmap /home/guni/.Xmodmap
 else
     echo "Your platform ($(uname -a)) is not supported."
     exit 1
