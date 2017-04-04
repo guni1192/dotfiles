7adfce770443cafb8a71bdb651afb280990d937e
guni973 <ad2314ce71926@gmail.com>
2017-04-04 11:19:32 +0900

    unuse file delete and merge ln script

--- makelink.sh
+++ makelink.sh
@@ -1,15 +1,19 @@
 if [ "$(expr substr $(uname -s) 1 5)" == 'Linux' ]; then
+    # Zsh
     ln -s ~/dotfiles/.Xdefaults ~/.Xdefaults
     ln -s ~/dotfiles/.zshrc ~/.zshrc
     ln -s ~/dotfiles/.zshrc_base ~/.zshrc_base
     ln -s ~/dotfiles/.zshrc_linux ~/.zshrc_os
+    # Xmonad
     ln -s ~/dotfiles/.xmonad ~/.xmonad
+    # NeoVim
+    ln -s ~/dotfiles/nvim ~/.config/nvim
+    # Emacs
     mkdir ~/.emacs.d
     git clone https://github.com/dimitri/el-get.git ~/.emacs.d/el-get/
     ln -s ~/dotfiles/init.el ~/.emacs.d/init.el
+    # Tmux
     ln -s ~/dotfiles/.tmux.conf ~/.tmux.conf
-    # If use ThinkPad, .Xmodmap added
-    # ln -s /home/guni/dotfiles/.Xmodmap /home/guni/.Xmodmap
 else
     echo "Your platform ($(uname -a)) is not supported."
     exit 1
