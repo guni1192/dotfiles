457b67f3f21de205eaf7654037449c56c53763af
guni973 <ad2314ce71926@gmail.com>
2017-09-03 12:21:46 +0900

    asdf-vm env value changed

--- asdf_init.sh
+++ asdf_init.sh
@@ -9,18 +9,14 @@ asdf plugin-update --all
 export GNUPGHOME="${ASDF_DIR:-$HOME/.asdf}/keyrings/nodejs" && mkdir -p "$GNUPGHOME" && chmod 0700 "$GNUPGHOME"
 bash ~/.asdf/plugins/nodejs/bin/import-release-team-keyring
 
-GLOBAL_NODE_VERSION="8.3.0"
-GLOBAL_PYTHON_VERSION="3.6.2"
-GLOBAL_RUBY_VERSION="2.4.1"
-GLOBAL_HASKELL_VERSION="8.0.2"
 
-asdf install nodejs $GLOBAL_NODE_VERSION
-asdf install python $GLOBAL_PYTHON_VERSION
-asdf install ruby $GLOBAL_RUBY_VERSION
-asdf install haskell $GLOBAL_HASKELL_VERSION
+asdf install nodejs   $ASDF_PYTHON_VERSION
+asdf install python   $ASDF_NODEJS_VERSION
+asdf install haskell  $ASDF_HASKELL_VERSION
+asdf install ruby     $ASDF_Ruby_VERSION
 
-asdf global nodejs $GLOBAL_NODE_VERSION
-asdf global python $GLOBAL_PYTHON_VERSION
-asdf global ruby $GLOBAL_RUBY_VERSION
-asdf global haskell $GLOBAL_HASKELL_VERSION
+asdf global nodejs    $ASDF_PYTHON_VERSION
+asdf global python    $ASDF_NODEJS_VERSION
+asdf global haskell   $ASDF_HASKELL_VERSION
+asdf global ruby      $ASDF_Ruby_VERSION
 
