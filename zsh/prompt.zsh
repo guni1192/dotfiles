c7426508154504a28bda9a78a3a578ba05273e85
Takashi IIGUNI <iiguni.tks@gmail.com>
2021-03-11 07:44:23 +0900

    change prompt format

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -1,6 +1,6 @@
 autoload -Uz colors
 
-username="%{$fg[white]%}%{$bg[blue]%} %m@%n %{$reset_color%}%{$bg[white]%}%{$fg[blue]%}%{$reset_color%}"
+username="%{$fg[white]%}%{$bg[blue]%} %n@%m %{$reset_color%}%{$bg[white]%}%{$fg[blue]%}%{$reset_color%}"
 directory="%{$bg[white]%}%{$fg[black]%} %~ %{$reset_color%}%{$reset_color%}%{$bg[black]%}%{$fg[white]%}%{$reset_color%}"
 date="%{$fg[white]%}%{$bg[black]%} %D %{$reset_color%}%{$fg[black]%} %{$reset_color%}"
 return_code="%{$fg[white]%}%{$bg[black]%}  %?  %{$reset_color%}%{$fg_no_bold[black]%}%{$reset_color%}"
