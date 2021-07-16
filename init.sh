6a57fb556af1a56e53e5c4017f222128e4623802
Takashi IIGUNI <iiguni.tks@gmail.com>
2021-07-16 21:56:08 +0900

    [init] diable GUI Application

--- init.sh
+++ init.sh
@@ -3,60 +3,50 @@
 set -eu
 
 setup_zsh() {
-    if [ -e $HOME/.zsh ]; then
-        rm -rf ~/.zsh
-    fi
-    ln -sv ~/dotfiles/zsh ~/.zsh
-    ln -sv ~/.zsh/.zshrc ~/.zshrc
-    ln -sv ~/.zsh/.zshenv ~/.zshenv
+    rm -rf ~/.zsh
+
+    ln -fsv ~/dotfiles/zsh ~/.zsh
+    ln -fsv ~/.zsh/.zshrc ~/.zshrc
+    ln -fsv ~/.zsh/.zshenv ~/.zshenv
 }
 
 setup_vim() {
-    ln -sv ~/dotfiles/vimrc ~/.vimrc
     rm -rf ~/.vim/config
     mkdir -p ~/.vim
-    ln -sv ~/dotfiles/vim ~/.vim/config
+
+    ln -fsv ~/dotfiles/vimrc ~/.vimrc
+    ln -fsv ~/dotfiles/vim ~/.vim/config
 }
 
 setup_neovim() {
     mkdir -p ~/.config/nvim
+
     ln -sv ~/dotfiles/vimrc ~/.config/nvim/init.vim
 }
 
-if [ "$(expr substr $(uname -s) 1 5)" == 'Linux' ]; then
 
-    if [ ! -e $HOME/.config ]; then
-        mkdir $HOME/.config
-    fi
+# read -p "Do you use X11?[y/n] " yn ;
+# case $yn in
+#     [Yy]* ) {
+#         # i3 Window Manager
+#         echo "i3wm linking ..."
+#         ln -sv ~/dotfiles/i3 ~/.config/i3/
+#         echo "Xresources linking ..."
+#         ln -sv ~/dotfiles/.Xresources ~/.Xresources
+#         echo "alacritty linking ..."
+#         ln -sv ~/dotfiles/alacritty ~/.config/alacritty
+#         # polybar
+#         echo "polybar linking ..."
+#         ln -sv ~/dotfiles/polybar ~/.config/polybar/
+#     };;
+# esac
 
-    read -p "Do you use X11?[y/n] " yn ;
-    case $yn in
-        [Yy]* ) {
-            # i3 Window Manager
-            echo "i3wm linking ..."
-            ln -sv ~/dotfiles/i3 ~/.config/i3/
-            echo "Xresources linking ..."
-            ln -sv ~/dotfiles/.Xresources ~/.Xresources
 
-            echo "alacritty linking ..."
-            ln -sv ~/dotfiles/alacritty ~/.config/alacritty
-
-            # polybar
-            echo "polybar linking ..."
-            ln -sv ~/dotfiles/polybar ~/.config/polybar/
-        };;
-    esac
-
-elif [ "$(expr substr $(uname -s) 1 5)" == 'Darwin' ]; then
-    echo "setup macOS"
-else
-    echo "Your platform ($(uname -a)) is not supported."
-    exit 1
-fi
+mkdir -p $HOME/.config
 
 setup_zsh
 setup_vim
-setup_nvim
+setup_neovim
 
-ln -sv ~/dotfiles/gitconfig ~/.gitconfig
-ln -sv ~/dotfiles/.tmux.conf ~/.tmux.conf
+ln -fsv ~/dotfiles/gitconfig ~/.gitconfig
+ln -fsv ~/dotfiles/.tmux.conf ~/.tmux.conf
