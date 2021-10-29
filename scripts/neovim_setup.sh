232b4dd8173c57e567c3cbe5432a3160e9927ff7
Takashi IIGUNI <iiguni.tks@gmail.com>
2021-10-29 11:24:35 +0000

    add workflow file
    Signed-off-by: Takashi IIGUNI <iiguni.tks@gmail.com>

---
+++ scripts/neovim_setup.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+set -euxo pipefail
+
+sudo apt-get update -y
+
+sudo apt-get install -y \
+    ninja-build \
+    gettext \
+    libtool \
+    libtool-bin \
+    autoconf \
+    automake \
+    cmake \
+    g++ \
+    pkg-config \
+    unzip \
+    curl \
+    doxygen
+
+git clone https://github.com/neovim/neovim $HOME/neovim-src
+cd $HOME/neovim-src
+
+make CMAKE_INSTALL_PREFIX=/home/guni/.local
+make install
