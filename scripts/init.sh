6f972772da069f09eaf80b6cccada1a95318c3cf
Takashi IIGUNI <iiguni.tks@gmail.com>
2022-06-05 15:26:45 +0900

    zplug -> zinit
    Signed-off-by: Takashi IIGUNI <iiguni.tks@gmail.com>

--- scripts/init.sh
+++ scripts/init.sh
@@ -6,20 +6,25 @@ export XDG_CONFIG_HOME=$HOME/.config
 export XDG_CACHE_HOME=$HOME/.cache
 export XDG_DATA_HOME=$HOME/.local/share
 export XDG_STATE_HOME=$HOME/.local/state
+export PATH=$HOME/.local/bin:$PATH
 
 mkdir -p $XDG_CONFIG_HOME
+mkdir -p $XDG_DATA_HOME
 
 setup_zsh() {
     ln -fsv ~/dotfiles/zshenv ~/.zshenv
     ln -fsv ~/dotfiles/zsh/ $XDG_CONFIG_HOME/zsh
     mkdir -p $XDG_DATA_HOME/zsh
+
+    ZINIT_HOME="${XDG_DATA_HOME:-${HOME}/.local/share}/zinit/zinit.git"
+    mkdir -p "$(dirname $ZINIT_HOME)"
+    git clone https://github.com/zdharma-continuum/zinit.git "$ZINIT_HOME"
 }
 
 setup_neovim() {
     ln -fsv ~/dotfiles/nvim/ $XDG_CONFIG_HOME/nvim
     sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
         https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
-
 }
 
 setup_tmux() {
@@ -37,7 +42,7 @@ setup_aqua() {
 }
 
 setup_rust() {
-    curl https://sh.rustup.rs -sSf | sh -s -- --default-toolchain stable
+    curl https://sh.rustup.rs -sSf | sh -s -- -y --default-toolchain stable
 }
 
 setup_zsh
