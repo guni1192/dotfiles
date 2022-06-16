d07dc713aed2a2c5006d6f0b31f1dde040dc4240
Takashi IIGUNI <iiguni.tks@gmail.com>
2022-06-16 04:26:56 +0000

    neovim plug install when execute setup script
    Signed-off-by: Takashi IIGUNI <iiguni.tks@gmail.com>

--- scripts/init.sh
+++ scripts/init.sh
@@ -25,6 +25,8 @@ setup_neovim() {
     ln -fsv ~/dotfiles/nvim/ $XDG_CONFIG_HOME/nvim
     sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
         https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+
+    nvim --headless +PlugInstall +qa
 }
 
 setup_tmux() {
