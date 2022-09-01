b670c6f233b79baacd0a0200f14933f2eb843f59
Takashi IIGUNI <iiguni.tks@gmail.com>
2022-09-01 11:23:52 +0000

    update zsh prompt
    Signed-off-by: Takashi IIGUNI <iiguni.tks@gmail.com>

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -1,9 +1,5 @@
 autoload -Uz colors
 
-username="%F{black}%K{cyan} %n@%m %b%k"
-directory="%F{black}%K{green} %~ %b%k"
-return_code="%F{black}%K{white} %? %b%k"
-auth="%F{white}%K{black}%# %b%k"
 
 autoload -Uz vcs_info
 setopt prompt_subst
@@ -13,11 +9,20 @@ zstyle ':vcs_info:git:*'  unstagedstr   "%F{red}+"
 zstyle ':vcs_info:*'      formats       "%F{black}%K{white} > %b %k"
 zstyle ':vcs_info:*'      actionformats '%b|%a'
 
+GIT_CONFIG_USER_NAME=$(git config --get user.name)
+GIT_CONFIG_USER_EMAIL=$(git config --get user.email)
+GIT_USER="%F{white}%K{gray} ${GIT_CONFIG_USER_NAME}<${GIT_CONFIG_USER_EMAIL}> %b%k"
+
+username="%F{black}%K{cyan} %n@%m %b%k"
+directory="%F{black}%K{green} %~ %b%k"
+return_code="%F{black}%K{white} %? %b%k"
+auth="%F{white}%K{black}%# %b%k"
+
 precmd () {
   vcs_info
 }
 
-PROMPT="$username$directory$return_code
+PROMPT="$username$directory$GIT_USER$return_code
 $auth"
 
 RPROMPT='${vcs_info_msg_0_}'
