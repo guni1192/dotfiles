0410d450d1f2309c04f9f140f8ea13a7688c9679
Takashi IIGUNI <takashi.iiguni@guni.dev>
2023-10-22 16:27:37 +0000

    fix colorcheme
    Signed-off-by: Takashi IIGUNI <takashi.iiguni@guni.dev>

--- nvim/lua/color.lua
+++ nvim/lua/color.lua
@@ -1,17 +1,4 @@
 vim.cmd("syntax on")
-vim.cmd("autocmd ColorScheme * highlight Normal ctermbg=none")
-vim.cmd("autocmd ColorScheme * highlight LineNr ctermbg=none")
-
--- " background clear
--- augroup Myhighlight
---     autocmd VimEnter,ColorScheme * highlight Normal ctermbg=none
---     autocmd VimEnter,ColorScheme * highlight LineNr ctermbg=none
---     autocmd VimEnter,ColorScheme * highlight SignColumn ctermbg=none
---     autocmd VimEnter,ColorScheme * highlight VertSplit ctermbg=none
---     autocmd VimEnter,ColorScheme * highlight NonText ctermbg=none 
--- augroup END
-
-
 vim.api.nvim_create_augroup('BgColor', {})
 vim.api.nvim_create_autocmd({'VimEnter', 'ColorScheme'}, {
     pattern = {'*'},
