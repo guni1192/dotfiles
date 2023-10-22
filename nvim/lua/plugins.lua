431dfc302432d1741e168553ed7f7fe28c25265d
Takashi IIGUNI <takashi.iiguni@guni.dev>
2023-10-23 00:08:31 +0900

    install nvim-tree
    Signed-off-by: Takashi IIGUNI <takashi.iiguni@guni.dev>

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -10,3 +10,41 @@ if not vim.loop.fs_stat(lazypath) then
   })
 end
 vim.opt.rtp:prepend(lazypath)
+
+local lazy = require('lazy')
+
+lazy.setup({{
+  "nvim-tree/nvim-tree.lua",
+  version = "*",
+  lazy = false,
+  dependencies = {
+    "nvim-tree/nvim-web-devicons",
+  },
+  config = function()
+    local function my_on_attach(bufnr)
+      local api = require "nvim-tree.api"
+
+      local function opts(desc)
+        return { desc = "nvim-tree: " .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
+      end
+
+      -- default mappings
+      api.config.mappings.default_on_attach(bufnr)
+
+      -- custom mappings
+      vim.keymap.set('n', '<C-t>', api.tree.change_root_to_parent,        opts('Up'))
+      vim.keymap.set('n', '?',     api.tree.toggle_help,                  opts('Help'))
+      vim.keymap.set('n', '<C-e>', api.tree.toggle,                       opts('Toggle'))
+    end
+    require("nvim-tree").setup {
+        on_attach = my_on_attach,
+    }
+  end,
+},
+{"freeo/vim-kalisi"},
+})
+
+
+-- nvim-tree global keymap
+local api = require "nvim-tree.api"
+vim.keymap.set('n', '<C-e>', api.tree.toggle)
