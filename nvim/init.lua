9bff1d3820766f7b2154dc048b53de11f2b8bf72
Takashi IIGUNI <takashi.iiguni@guni.dev>
2023-10-22 23:55:57 +0900

    basic neovim configuration convert vimscript to lua
    Signed-off-by: Takashi IIGUNI <takashi.iiguni@guni.dev>

---
+++ nvim/init.lua
@@ -0,0 +1,3 @@
+
+require('editor')
+require('plugins')
---
+++ nvim/lua/editor.lua
@@ -0,0 +1,31 @@
+vim.opt.number = true
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.expandtab = true
+vim.opt.shiftwidth = 4
+vim.opt.smartindent = true
+vim.opt.autoindent = true
+vim.opt.list = true
+-- vim.opt.listchars=tab:>-,trail:-,eol:↲,extends:»,precedes:«,nbsp:%
+vim.opt.hidden = true
+vim.opt.history = 50
+vim.opt.virtualedit = "block"
+vim.opt.whichwrap = "b,s,h,l,<,>,[,]"
+vim.opt.backspace = "indent,eol,start"
+vim.opt.wildmenu = true
+vim.opt.clipboard = "unnamed"
+vim.opt.pumheight = 10
+vim.opt.showmode = true
+vim.opt.wildmode = "list:full"
+vim.opt.encoding = "utf-8"
+vim.opt.helplang = "ja,en"
+vim.opt.cursorline = true
+
+-- inoremap jj <ESC>`^
+
+vim.opt.sh=zsh
+-- tnoremap <silent> <ESC> <C-\><C-n>
+-- tnoremap <silent> jj <C-\><C-n>
+
+vim.opt.foldmethod = "syntax"
+vim.opt.foldlevel = 100
