911a03d753340add6845e1d12a1cfac04f5589fa
Takashi IIGUNI <takashi.iiguni@guni.dev>
2023-10-24 03:32:07 +0900

    update basic neovim config
    Signed-off-by: Takashi IIGUNI <takashi.iiguni@guni.dev>

--- nvim/lua/editor.lua
+++ nvim/lua/editor.lua
@@ -6,7 +6,7 @@ vim.opt.shiftwidth = 4
 vim.opt.smartindent = true
 vim.opt.autoindent = true
 vim.opt.list = true
--- vim.opt.listchars=tab:>-,trail:-,eol:↲,extends:»,precedes:«,nbsp:%
+vim.opt.listchars= 'tab:>-,trail:-,eol:↲,extends:»,precedes:«,nbsp:%'
 vim.opt.hidden = true
 vim.opt.history = 50
 vim.opt.virtualedit = "block"
@@ -18,14 +18,7 @@ vim.opt.pumheight = 10
 vim.opt.showmode = true
 vim.opt.wildmode = "list:full"
 vim.opt.encoding = "utf-8"
-vim.opt.helplang = "ja,en"
+vim.opt.helplang = "en"
 vim.opt.cursorline = true
-
--- inoremap jj <ESC>`^
-
--- vim.opt.sh=zsh
--- tnoremap <silent> <ESC> <C-\><C-n>
--- tnoremap <silent> jj <C-\><C-n>
-
 vim.opt.foldmethod = "syntax"
 vim.opt.foldlevel = 100
