03cf5b200bcc9f1328e6d630db0914ca0b5202c6
Takashi IIGUNI <takashi.iiguni@guni.dev>
2024-05-25 17:57:13 +0900

    enable copilot
    Signed-off-by: Takashi IIGUNI <takashi.iiguni@guni.dev>

--- nvim/lua/lsp.lua
+++ nvim/lua/lsp.lua
@@ -113,8 +113,8 @@ lspconfig.clangd.setup{
 }
 
 -- GitHub Copilot
--- vim.keymap.set('i', '<C-g>', 'copilot#Accept("<CR>")', {
---   expr = true,
---   replace_keycodes = false
--- })
--- vim.g.copilot_no_tab_map = true
+vim.keymap.set('i', '<C-g>', 'copilot#Accept("<CR>")', {
+  expr = true,
+  replace_keycodes = false
+})
+vim.g.copilot_no_tab_map = true
