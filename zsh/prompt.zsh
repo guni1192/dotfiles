3341fd43f5230bd3c8d2545e97164c54158bf005
Takashi Iiguni <takashi.iiguni@dr-ubie.com>
2025-01-10 20:24:09 +0900

    fix zsh prompt
    Signed-off-by: Takashi Iiguni <takashi.iiguni@dr-ubie.com>

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -8,19 +8,19 @@ zstyle ':vcs_info:git:*'  unstagedstr   "%F{red}+"
 zstyle ':vcs_info:*'      formats       "%F{green} %b%k"
 zstyle ':vcs_info:*'      actionformats '%b|%a'
 
-username="%F{blue}%K{black}%n@%m%b%k"
-directory="%F{cyan}%K{black} %~%b%k"
-return_code="%F{white}%K{black} %? %b%k"
-auth="%F{white}%K{black}%# %b%k"
+username="%F{blue}%n@%m%b%k"
+directory="%F{cyan} %~%b%k"
+return_code="%F{white} %? %b%k"
+auth="%F{white}%# %b%k"
 
 precmd () {
   vcs_info
   export kube_current_context=""
   context=$(kubectl config current-context)
   if [[ "$context" =~ .*"prd".* ]] || [[ "$context" =~ .*"production".* ]]; then
-      export kube_current_context="%F{red}%K{black} $context %b%k"
+      export kube_current_context="%F{red} $context %b%k"
   else
-      export kube_current_context="%F{yellow}%K{black} $context %b%k"
+      export kube_current_context="%F{yellow} $context %b%k"
   fi
 }
 
