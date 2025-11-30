1ea06316319f032dd3d9930fd55975255096fb7e
Takashi IIGUNI <takashi.iiguni@guni.dev>
2025-11-30 16:27:36 +0900

    remove old config
    Signed-off-by: Takashi IIGUNI <takashi.iiguni@guni.dev>

--- wezterm/wezterm.lua
+++
@@ -1,8 +0,0 @@
-local wezterm = require 'wezterm'
-
-local config = wezterm.config_builder()
-config.color_scheme = 'hardcore'
-
-config.hyperlink_rules = wezterm.default_hyperlink_rules()
-
-return config
