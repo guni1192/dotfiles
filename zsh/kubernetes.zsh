0b195309a9f92cd614baf72188bbd79fb4b3f64a
Takashi IIGUNI <takashi.iiguni@guni.dev>
2025-11-30 16:15:47 +0900

    check kubectl exists
    Signed-off-by: Takashi IIGUNI <takashi.iiguni@guni.dev>

--- zsh/kubernetes.zsh
+++ zsh/kubernetes.zsh
@@ -3,7 +3,7 @@ alias kgp='kubectl get pods'
 alias kgs='kubectl get service'
 alias kgi='kubectl get ingress'
 
-if kubectl &> /dev/null; then
+if command -v kubectl > /dev/null 2>&1; then
     source <(kubectl completion zsh)
     compdef __start_kubectl k
 fi
