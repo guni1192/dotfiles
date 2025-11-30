f28c42d582079d5e6f0ed6fd34b560422112e41f
Takashi IIGUNI <takashi.iiguni@guni.dev>
2025-11-30 15:23:29 +0900

    fix neovim installer scripts
    Signed-off-by: Takashi IIGUNI <takashi.iiguni@guni.dev>

--- scripts/neovim-installer.sh
+++ scripts/neovim-installer.sh
@@ -3,6 +3,6 @@ set -e
 
 tmpdir=$(mktemp -d)
 mkdir -p "$HOME/.local"
-curl -L https://github.com/neovim/neovim/releases/latest/download/nvim-linux64.tar.gz | tar xz -C "$tmpdir"
-ls -al "$tmpdir/nvim-linux64"
-mv $tmpdir/nvim-linux64/* "$HOME/.local/"
+curl -L https://github.com/neovim/neovim/releases/latest/download/nvim-linux-x86_64.tar.gz | tar xz -C "$tmpdir"
+ls -al "$tmpdir/nvim-linux-x86_64"
+mv $tmpdir/nvim-linux-x86_64/* "$HOME/.local/"
